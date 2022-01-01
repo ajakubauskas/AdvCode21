2b68f3eba4e9e52258662d848fbdb59dc10ba97f
aivaras <aivaras@igeolise.com>
2022-01-01 18:23:08 +0200

    task 4 part 1

--- task4/src/main/scala/part1.scala
+++ task4/src/main/scala/part1.scala
@@ -1,27 +1,93 @@
+import scala.collection.mutable
 import scala.io.Source
 
 object part1 {
 
   def main(args: Array[String]): Unit = {
 
-    val src = Source.fromFile("example")
+    val src = Source.fromFile("input.part1")
 
-    val lines = src.getLines().toList
+    val lines = src.getLines()
+
+    val numbers = lines.next().split(",").map(_.toInt)
+
+    val builder = new mutable.ArrayBuilder.ofRef[BingoBoard]()
+
+    while(lines.hasNext) {
+      builder.addOne(BingoBoard.parse(lines))
+    }
+
+    val boards = builder.result()
+
+
+
+    for (n <- numbers) {
+
+      boards.find { board =>
+        board.enterAndCheck(n)
+      }.foreach { board =>
+        println(board.sumUnmarked * n)
+        return
+      }
+    }
+
+
 
   }
 
   case class BingoBoard(rows: Array[Array[Int]]) {
 
-    private val marked = Array.fill(4)(Array.fill(4)(false))
+    private val marked: Array[Array[Boolean]] = Array.fill(5)(Array.fill(5)(false))
 
-    def check(): Boolean = ???
+    private def winnerRow(markedRows: Array[Array[Boolean]]) = markedRows.exists(_.forall(_ == true))
 
+    private def isWinner = winnerRow(marked) || winnerRow(marked.transpose)
 
-    def enter(x: Int) = {
-      ???
+    def enter(x: Int): Unit = {
+      for (i <- 0 until 5; j <- 0 until 5) {
+        if (rows(i)(j) == x) {
+            marked(i)(j) = true
+            return
+          }
+      }
     }
 
-    def enterAndCheck(x: Int): Boolean = ???
+    def sumUnmarked = {
+      val unmarked = for {
+        i <- 0 until 5
+        j <- 0 until 5
+      } yield {
+        if (marked(i)(j))
+          0
+        else rows(i)(j)
+      }
+      unmarked.sum
+    }
+
+    def enterAndCheck(x: Int): Boolean = {
+      enter(x)
+      isWinner
+    }
+
+    override def toString: String = {
+      "--------------\n" +
+      rows.map(_.mkString(" ")).mkString("\n") +
+        "\n--------------"
+    }
+  }
+
+  object BingoBoard {
+    def parse(strs: Iterator[String]): BingoBoard = {
+      strs.next()
+
+      BingoBoard(Array(
+        strs.next().split(" ").filter(_.nonEmpty).map(_.toInt),
+        strs.next().split(" ").filter(_.nonEmpty).map(_.toInt),
+        strs.next().split(" ").filter(_.nonEmpty).map(_.toInt),
+        strs.next().split(" ").filter(_.nonEmpty).map(_.toInt),
+        strs.next().split(" ").filter(_.nonEmpty).map(_.toInt)
+      ))
+    }
   }
 
 }
