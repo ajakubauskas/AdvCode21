31404b2c28f347c92737fdd8deb4127bc78ab233
aivaras <aivaras@igeolise.com>
2022-01-04 12:49:13 +0200

    task7 init, part 1 incorrect

---
+++ task7/src/main/scala/main.scala
@@ -0,0 +1,31 @@
+import scala.io.Source
+
+object main {
+
+  def main(args: Array[String]): Unit = {
+
+    val src = Source.fromFile("input")
+
+    val str = src.getLines().next()
+
+    val crabs = str.split(",").map(_.toInt)
+
+    val min = crabs.min
+    val max = crabs.max
+
+    val distances = Array.fill(max - min + 1)(0)
+
+    for (i <- distances.indices) {
+      val offset = i + min
+
+      distances(i) = crabs.map(c => math.abs(c - offset)).sum
+    }
+
+    val minDist = distances.min
+    println(minDist)
+
+    println(distances.indexWhere(_ == minDist) + min)
+
+  }
+
+}
