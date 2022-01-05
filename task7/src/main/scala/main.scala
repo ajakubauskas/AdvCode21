f5c12bb52dc4b350ee4c283f302557dce08bfc58
aivaras <aivaras@igeolise.com>
2022-01-05 12:55:10 +0200

    part 2 solved - involves some 5th grade knowledge!

--- task7/src/main/scala/main.scala
+++ task7/src/main/scala/main.scala
@@ -13,6 +13,8 @@ object main {
     val min = crabs.min
     val max = crabs.max
 
+    // part 1
+
     val distances = Array.fill(max - min + 1)(0)
 
     for (i <- distances.indices) {
@@ -24,6 +26,24 @@ object main {
     val minDist = distances.min
 
     println(minDist)
+
+    // part 2
+    // Sum 1 .. n is known as Newton's formula: S(n) = n(n+1) / 2
+    // however for performance reasons it would be more prudent to precompute a "distance table" -
+    // an array of cumulative distances where distTable(i) = i + distTable(i-1) and then do array lookups!
+
+    def newtSum(n: Int) = n*(n+1) / 2
+
+    for (i <- distances.indices) {
+      val offset = i + min
+
+      distances(i) = crabs.map(c => newtSum(math.abs(c - offset))).sum
+    }
+
+
+    val newMinDist = distances.min
+
+    println(newMinDist)
 
 
   }
