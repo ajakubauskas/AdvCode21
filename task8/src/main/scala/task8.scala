3da589568289917ef233334ef6a501c622ccc289
aivaras <aivaras@igeolise.com>
2022-01-06 12:20:15 +0200

    part 1 ez

--- task8/src/main/scala/task8.scala
+++ task8/src/main/scala/task8.scala
@@ -4,10 +4,16 @@ object task8 {
 
   def main(args: Array[String]) : Unit = {
 
-    val src = Source.fromFile("example")
+    val src = Source.fromFile("input")
 
     def knowns(s: String) =
       s.length == 2 || s.length == 4 || s.length == 3 || s.length == 7
+
+    val rhsSegments = src.getLines().map(_.split('|')).map(_.tail).flatMap(_.map(_.split(" ")))
+
+    println(
+      rhsSegments.map(_.count(knowns)).sum
+    )
 
 
   }
