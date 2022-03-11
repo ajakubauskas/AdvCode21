d034448107ed43a4ead9adb5e4747bfa0146f54f
ajakubauskas <aivaras@traveltime>
2022-03-11 18:37:19 +0200

    wip

--- task9/src/main/scala/task9.scala
+++ task9/src/main/scala/task9.scala
@@ -6,7 +6,41 @@ object task9 {
 
     val src = Source.fromFile("example.txt")
 
+    val lines = src.getLines().toArray.map { line =>
+      line.map(_.asDigit).toArray
+    }
 
+    val hm = new HeightMap(lines)
+
+    lines.head.indices.foreach { x =>
+      lines.indices.foreach { y =>
+        if (hm.isLowest(x, y))
+          println(lines(y)(x))
+      }
+
+    }
+
+
+
+
+  }
+
+  class HeightMap(lines: Array[Array[Int]]) {
+    val width = lines.head.length - 1
+    val height = lines.length - 1
+
+    def isLowest(x: Int, y: Int) = {
+      val left = lines(y)(x - 1 max 0)
+      val right = lines(y)(x + 1 min width)
+      val top = lines(y - 1 max 0)(x)
+      val bottom = lines(y + 1 min height)(x)
+
+      
+      lines(y)(x) < left &&
+        lines(y)(x) < right &&
+        lines(y)(x) < top &&
+        lines(y)(x) < bottom
+    }
   }
 
 }
